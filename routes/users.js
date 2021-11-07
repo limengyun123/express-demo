11e1132681f8e8195218a21a155fdc03d662d9c6
limengyun <limengyun@sics.ac.cn>
2021-11-07 09:53:00 +0800

    install dependencies

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
+var fs = require("fs");
+var multer = require("multer");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
+
+router.post(
+  "/upload",
+  multer({ dest: "tmp/" }).single("file"),
+  function (req, res, next) {
+    console.log("/upload", req.file);
+    res.send("上传成功！");
+  }
+);
 
 module.exports = router;
